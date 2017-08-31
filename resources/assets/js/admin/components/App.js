8a84df2d0ae73f9d0480a8481c18de33422e8fd7
Hiroki Takahashi <htakahashi@hyphenio.com.au>
2017-09-01 07:52:47 +1000

    Made the basic components

--- resources/assets/js/admin/components/App.js
+++ resources/assets/js/admin/components/App.js
@@ -6,6 +6,9 @@ import {
 }
 from 'react-router-dom';
 import Posts from './Posts';
+import Images from './Images';
+import Comments from './Comments';
+import Stats from './Stats';
 
 export default class App extends React.Component {
 	constructor(props) {
@@ -20,9 +23,21 @@ export default class App extends React.Component {
         				<li className="sidebar-list">
         					<NavLink to="/posts" style={{ textDecoration: 'none' }} activeStyle={{ color: 'red' }} replace>Posts</NavLink>
         				</li>
+        				<li className="sidebar-list">
+        					<NavLink to="/images" style={{ textDecoration: 'none' }} activeStyle={{ color: 'red' }} replace>Images</NavLink>
+        				</li>
+        				<li className="sidebar-list">
+        					<NavLink to="/comments" style={{ textDecoration: 'none' }} activeStyle={{ color: 'red' }} replace>Comments</NavLink>
+        				</li>
+        				<li className="sidebar-list">
+        					<NavLink to="/stats" style={{ textDecoration: 'none' }} activeStyle={{ color: 'red' }} replace>Stats</NavLink>
+        				</li>
         			</ul>
 					<div className="admin-contents">
 						<Route path="/posts" component={Posts}/>
+						<Route path="/images" component={Images}/>
+						<Route path="/comments" component={Comments}/>
+						<Route path="/stats" component={Stats}/>
 					</div>
 				</div>
 			</HashRouter>
---
+++ resources/assets/js/admin/components/Images.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class Images extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<div>
+			Images Component
+			</div>
+		);
+	}
+}
