1026db894a71b1d49f1579e2b46decfa4ad18496
Hiroki Takahashi <htakahashi@hyphenio.com.au>
2017-12-29 15:08:47 +1000

    Added Disqus

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -21,5 +21,21 @@
 	<p>Previous : <a href="/{{ $prev->url }}">{{ $prev->title }}</a></p>
 </div>
 @endif
+<div id="disqus_thread"></div>
+<script>
+var disqus_config = function () {
+this.page.url = "{{env('APP_URL').'/'.$post->url}}";  // Replace PAGE_URL with your page's canonical URL variable
+this.page.identifier = "{{env('APP_URL').'/'.$post->id}}"; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+};
+
+(function() { // DON'T EDIT BELOW THIS LINE
+	var d = document, s = d.createElement('script');
+	s.src = 'https://hiroki-com.disqus.com/embed.js';
+	s.setAttribute('data-timestamp', +new Date());
+	(d.head || d.body).appendChild(s);
+})();
+</script>
+<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+
 
 @endsection
